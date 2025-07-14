587ccd86caf21a9f753daf3e0c02127a15e156bb
Areeka Designer Wali <areekadesignerwali7500@gmail.com>
2025-07-14 22:29:09 +0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,10 @@
+<?php
+// Set the timezone to your location (e.g., 'America/New_York')
+date_default_timezone_set('America/New_York');
+
+// Get the current date and time in a readable format
+$current_time = date('h:i A T \o\n l, F d, Y');
+
+// Display the date and time in a paragraph tag
+echo "<p>Today's date and time is $current_time.</p>";
+?>
